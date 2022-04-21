18e50e29ec8fb241d88a68e24d14397ab9499778
vaasureddy <psriinu@gmail.com>
2022-04-21 18:57:48 +0530

    "this is commited by vaasu"

---
+++ sampl1.java
@@ -0,0 +1,64 @@
+package javaSimpleclasses;
+
+public class sampleclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// opeators
+		int a; // declare
+		a=5; // assign
+		// arthimaitc
+		// relational
+		//increment /decremennt
+		//binary
+		//locaal 
+		
+		int x,y;
+		x=10;
+		y=5;
+		int temp;
+		temp=x+y;
+		int multiplcation=x*y;
+		int substarction =x-y;
+		
+		System.out.println("additon of two no is: "+temp);
+		System.out.println("multiplcation of two no is: "+multiplcation);
+		System.out.println("substarction of two no is: "+substarction);
+		
+		// relational - and ,or,not
+		//comparison - <,>,<=,>=,!
+		
+		int first,second,third;
+		
+		first=10;
+		second=30;
+		third=50;
+		boolean status=true;
+		
+		if(first>second){
+			
+		}else if(second>first){
+			
+		}else if(first>=100){
+			
+		}else if(second<=20){
+			
+		}else if((first>second) && (first>third)){
+			
+		}else if(first !=second){
+			
+		}else if((first>second) || (first >third) || (third>first)){
+			
+			System.out.println("testing");
+			
+		}else if(!status){
+		
+		}
+		
+		
+
+	}
+
+}
+
